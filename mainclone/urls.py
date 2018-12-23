cc5c3543a4defd2ff62c380d9b435665f52792bc
HassibIbrahim <ihassib@gmail.com>
2018-12-24 01:43:06 +0300

    url depo

--- mainclone/urls.py
+++ mainclone/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^search/(?P<name>.+)$', views.find, name='save'),
     url(r'^follow_or_not/(?P<user_id>\d+)$', views.togglefollow, name='follow_or_not'),
     url(r'^unlike/(?P<post_id>\d+)$', views.unlike, name='unlike')
-]
+]      
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
