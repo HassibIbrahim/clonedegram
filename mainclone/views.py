8f9b45112f1374b7d3114c96d7037f020fb47103
HassibIbrahim <ihassib@gmail.com>
2018-12-24 01:35:43 +0300

    decos initializing

--- mainclone/views.py
+++ mainclone/views.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Profile,Post,Likes,Follows,Saves,Comment
 from django.contrib.auth.models import User
 from .forms import PostForm,CommentForm,ProfileForm
 from django.contrib.auth.decorators import login_required
-
-
 # Create your views here.
 
 
